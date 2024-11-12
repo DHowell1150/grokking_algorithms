b0627d89b011ce3a08de26957e026c6a45d9c8fe
Dana Howell <howelld115@gmail.com>
2024-11-12 16:31:12 -0700

    completes #binarysearch

---
+++ binary_search.rb
@@ -0,0 +1,22 @@
+require "pry"
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+def binary_search(arr, item_to_find) #on ordered list
+  low = 0
+  high = (arr.length) - 1  
+  while low <= high
+    mid = (high + low) / 2
+    guess = arr[mid]
+    if guess == item_to_find
+      return mid
+    elsif guess <= item_to_find
+      low = mid + 1
+    else #guess >= item_to_find
+      high = mid - 1
+    end
+  end
+end
+puts binary_search(arr, 9)
+
+
+#My question is why can't we use something more simple like .max?
